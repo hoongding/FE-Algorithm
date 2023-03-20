f3e03a566f3aceb1276e659374606cde254e02ac
JangHoonSeok <orirori512@naver.com>
2023-03-20 22:36:42 +0900

    [PGS] 기능개발 / Lv.2
    zip함수 구현해서 리팩토링 해보기

--- week1/[PGS] 기능개발.js
+++ week1/[PGS] 기능개발.js
@@ -4,9 +4,10 @@
 // 배포는 하루에 한번만, 95%, 4%면 배포는 2일뒤에 이루어짐.
 function solution(progresses, speeds) {
   let completeDay = [];
-  progresses.forEach((progress, idx) => {
-    completeDay.push(Math.ceil((100 - progress) / speeds[idx]));
-  });
+
+  for (const [progress, speed] of zip(progresses, speeds)) {
+    completeDay.push(Math.ceil((100 - progress) / speed));
+  }
 
   for (let i = 0; i < completeDay.length - 1; i++) {
     if (completeDay[i] > completeDay[i + 1])
@@ -14,9 +15,17 @@ function solution(progresses, speeds) {
   }
 
   const result = completeDay.reduce((acc, cur) => {
+    // 동일 요소 Count 하기
     acc[cur] = (acc[cur] || 0) + 1;
     return acc;
   }, {});
 
   return Object.values(result);
+}
+
+function* zip(a, b) {
+  // Generator로 구현..
+  // Python의 zip 함수 활용
+  const n = Math.min(a.length, b.length);
+  for (let i = 0; i < n; i++) yield [a[i], b[i]];
 }
