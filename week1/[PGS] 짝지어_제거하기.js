57f1f30c1dd3a9289919dc2c9d03025c60ae6439
JangHoonSeok <orirori512@naver.com>
2023-03-23 15:47:36 +0900

    ✨ feat : 짝지어 제거하기 리팩토링

--- week1/[PGS] 짝지어_제거하기.js
+++ week1/[PGS] 짝지어_제거하기.js
@@ -2,11 +2,10 @@
 // 아무것도 없으면 0을 return
 // 모두 제거 가능하면 1을 return
 function solution(s) {
-  const sArr = s.split('');
   const stack = [];
-  for (str of sArr) {
+  for (str of s) {
     if (stack.length === 0) stack.push(str);
-    else if (stack[stack.length - 1] !== str) stack.push(str);
+    else if (stack.at(-1) !== str) stack.push(str);
     else stack.pop();
   }
   return stack.length === 0 ? 1 : 0;
